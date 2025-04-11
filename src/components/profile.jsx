6617f1176ec449c638e3ed0d499a0e55b63f6efb
tanish tirpathi <tanishtirpathi0@gmail.com>
2025-04-12 05:13:05 +0530

    just complete the overview of the profile

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -32,8 +32,8 @@ function Profile() {
   }, []); // Empty dependency array ensures this runs once on mount
 
   return (
-    <div className="min-h-screen w-screen flex text-white bg-red font-sans">
-      <aside className="w-60 bg-gradient-to-b from-zinc-900 to-red-900 p-4 border border-gray-200">
+    <div className="min-h-screen w-screen flex text-white bg-red font-san ">
+      <aside className="w-60 bg-gradient-to-b from-black-900 to-red-900 p-4 overflow-hidden ">
         <div className="bg-gray-300 w-14 h-8 mb-6" />
         <nav className="flex flex-col gap-7 text-sm">
           <a
@@ -68,7 +68,7 @@ function Profile() {
           </a>
         </nav>
       </aside>
-      <main className="flex-1 p-4 bg-gradient-to-br from-black to-red-900 relative">
+      <main className="overflow-hidden flex-1 p-4 bg-gradient-to-br from-black to-red-900 relative">
         {loading && <p>Loading...</p>}
         {error && <p className="text-red-500">{error}</p>}
         
@@ -86,8 +86,8 @@ function Profile() {
             </div>
           </div>
         )}
-    <div className="">  
-       <div className=""> 
+    <div className="flex">  
+       <div className="w-full"> 
         <div className="flex items-start justify-center flex-col p-4">
           <h2>Links</h2>
           <a href="#" className="text-blue visited:text-white no-underline">
@@ -134,11 +134,26 @@ function Profile() {
           className="w-full p-3 border-2 border-blue-500 rounded focus:outline-none focus:ring-1 focus:ring-blue-500"
           value={description}
           onChange={(e) => setDescription(e.target.value)}
-          rows={3}
+          rows={4}
+        />
+      </div>
+      <div className="relative w-full m-3">
+        <label 
+          className="absolute -top-2 left-2 px-1 text-xs font-medium bg-black text-blue-500 " 
+          htmlFor="description"
+        >
+          Posts
+        </label>
+        <textarea
+          id="description" placeholder="your posted posts"
+          className="w-full p-3 border-2 border-blue-500 rounded focus:outline-none focus:ring-1 focus:ring-blue-500"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          rows={2}
         />
       </div>
     </div>
-    <img src="./image2.png" alt="" /> </div></div>
+     </div><img src="./image11.png" className=" ml-30 h-130" alt="" /></div>
       </main>
     </div>   
 
