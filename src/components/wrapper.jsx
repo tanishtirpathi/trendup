f71f6086e3d1e6593ab13f18b2ef56c61f0e8c09
tanish tirpathi <tanishtirpathi0@gmail.com>
2025-04-14 15:21:49 +0530

    almost compltet the layout page

--- src/components/wrapper.jsx
+++ src/components/wrapper.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import TextPressure from "./animation/TextPressure";
 import CountUp from "./animation/counup";
 import { InteractiveHoverButton } from "./magicui/interactive-hover-button";
+import "./wrapper.css";
 function Wrapper() {
   return (
     <div className="bg-black w-screen h-screen">
@@ -21,18 +22,31 @@ function Wrapper() {
           minFontSize={6}
         />
       </div>
-      <div className="flex items-center justify-center" style={{ margin: "-100px 100px  " }}>
+      <div
+        className="flex items-center justify-center"
+        style={{ margin: "-100px 100px  " }}
+      >
         <h1 className="text-2xl text-white">
-          
-        Welcome to Trend Up — the social media space where trends take flight.
+          Welcome to Trend Up — the social media space where trends take flight.
           Discover what's buzzing, follow real-time hype, and express yourself
           in a place built entirely with sleek, responsive frontend magic.
-
         </h1>
       </div>
-      <div id="button">
-      <InteractiveHoverButton>Interactive Hover Button</InteractiveHoverButton>
-
+      <div
+        id="button"
+        className="flex items-center justify-center "
+        style={{ margin: "240px 300px  " }}
+      >
+        <InteractiveHoverButton className= "pl-20 pr-20 m-10"
+          onClick={() => (window.location.href = "login")}
+        >
+          Login
+        </InteractiveHoverButton>
+        <InteractiveHoverButton className= "pl-20 pr-20 m-10"
+          onClick={() => (window.location.href = "signup")}
+        >
+          Sign Up
+        </InteractiveHoverButton>
       </div>
     </div>
   );
