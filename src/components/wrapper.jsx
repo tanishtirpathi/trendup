eecdaeba0c53fbcca2928ce0e197a656c416f0b4
tanish tirpathi <tanishtirpathi0@gmail.com>
2025-04-14 14:05:02 +0530

    working on the layout page

--- src/components/wrapper.jsx
+++ src/components/wrapper.jsx
@@ -1,14 +1,26 @@
 import React from "react";
+import TextPressure from "./animation/TextPressure";
+
 function Wrapper() {
   return (
     <>
-      <h1>hi my name is tanish stirapthi</h1>
-      <a href="/signup" id="signup">
-        Sign up
-      </a>
-      <a href="/Login" id="signup">
-        Sign <logi></logi>
-      </a>
+    <div className="bg-black w-screen h-screen ">
+    <div style={{position: 'relative', height: '300px',margin: '0px 300px '} }>
+  <TextPressure
+    text="Trend up !"
+    flex={true}
+    alpha={false}
+    stroke={false}
+    width={true}
+    weight={true}
+    italic={true}
+    textColor="#ffffff"
+    strokeColor="#ff0000"
+    minFontSize={6}
+  />
+</div>
+    </div>
+
     </>
   );
 }
