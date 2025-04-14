84a04836b006284df888439b9d52ad05fe3514e1
tanish tirpathi <tanishtirpathi0@gmail.com>
2025-04-15 02:26:25 +0530

    complete the login now the sign up side

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -7,7 +7,7 @@ export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [message, setMessage] = useState("");
-
+  
   const handleLogin = async (e) => {
     e.preventDefault();
     if (email === "tanishtirpathi@gmail.com" && password === "Rattangarh0011") {
@@ -19,6 +19,10 @@ export default function Login() {
       setMessage("❌ Invalid detail. Please try again.");
     }
   };
+  
+  const handleSignUp = () => {
+    window.location.href = "/signup";
+  };
 
   return (
     <div className="flex justify-around items-center h-screen w-screen overflow-scroll bg-gradient-to-br from-neutral-900 via-black to-blue-950 text-white gap-8 p-4">
@@ -27,7 +31,7 @@ export default function Login() {
       </div>
       
       <form onSubmit={handleLogin} className="flex flex-col justify-center items-center p-5 gap-4 w-72">
-        <h2 className="font-['Syne'] text-xl">Login to TrendUp</h2>
+        <h2 className="font-bold text-xl">Login to TrendUp</h2>
         <input
           type="email"
           placeholder="Email / username"
@@ -44,24 +48,27 @@ export default function Login() {
           required
           className="p-2 text-base rounded-lg border-none w-full bg-neutral-800 text-gray-200"
         />
-            <ShimmerButton   type="submit"
-          className="pl-14 pr-14 bg-transparent">  
+        
+        <ShimmerButton 
+          type="submit"
+          className="pl-14 pr-14 bg-transparent"
+        >
           Login on TrendUp
-     </ShimmerButton>
+        </ShimmerButton>
+        
         {message && (
           <div className="mt-1 p-2 rounded-lg text-gray-100 font-bold text-sm text-center">
             {message}
           </div>
         )}
-
-     <p>forget password</p>
-
+        
+        <p className="text-gray-300 hover:text-white cursor-pointer">Forgot password?</p>
+        
         <p className="text-blue-500">Don't have an account?</p>
-      <ShinyButton>  <a 
-          href="/signup" className="text-white"
-        >
-          Sign up
-        </a></ShinyButton>
+        
+        <ShinyButton onClick={handleSignUp}>
+       <p className="text-white">  Sign up</p> 
+        </ShinyButton>
       </form>
       
       <BorderBeam
