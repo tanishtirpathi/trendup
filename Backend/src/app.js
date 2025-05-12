791f893a939273ffea694dcd914d2d7618df185a
tanish tirpathi <tanishtirpathi0@gmail.com>
2025-05-12 15:35:00 +0530

    just start server

---
+++ Backend/src/app.js
@@ -0,0 +1,17 @@
+import express from "express"
+import cookieParser from "cookie-parser"
+import cors from 'cors'
+const app = express()
+   
+
+app.use(cors(
+   { origin:process.env.CORS_ORIGIN,
+    Credential:true 
+   }
+))
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(express.static("public"))
+app.use(cookieParser())
+
+export {app}
---
+++ Backend/src/index.js
@@ -0,0 +1,13 @@
+import { app } from "./app.js";
+import dotenv from "dotenv"
+import connectDB from "./src/db/index.js";
+dotenv. config({
+    path:'./.env'
+})
+
+connectDB()
+  .then(() =>
+    app.listen(process.env.PORT, () => console.log("listining the port at 7000"))
+  ).catch((err)=>{
+  console.log("MONGO DB connnection failed bro now what to do", err)
+});
